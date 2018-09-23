61ff6a2a457541b8dec2a435bbc58e9a07f369a0
arpansac <arpansac@gmail.com>
2018-09-23 12:53:02 +0530

    add speaker cards, complete the design

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -2,7 +2,15 @@ module UrlHelper
 
   def complete_url url_string
     unless url_string[/\Ahttp:\/\//] || url_string[/\Ahttps:\/\//]
-      url_string = "http://#{self.url}"
+      url_string = "http://#{url_string}"
+    end
+    return url_string
+  end
+
+
+  def twitter_url handle
+    if handle[0] == '@'
+      url_string = "http://twitter.com/#{handle}"
     end
     return url_string
   end
