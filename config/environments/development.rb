6b4d24f323ea11481d61e46a51f4770ab0b0ea37
arpansac <arpansac@gmail.com>
2018-10-16 13:37:33 +0530

    add sendgrid to production emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,11 +71,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.sendgrid.com',
+      address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'gdgnd.org',
-      user_name:            ENV["GDG_SENDGRID_USERNAME"],
-      password:             ENV["GDG_SENDGRID_PASSWORD"],
+      domain:               'localhost:3000',
+      user_name:            'gdgnewdelhi@gmail.com',
+      password:             '***REMOVED***',
       authentication:       'plain',
       enable_starttls_auto: true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,13 +95,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: 'gdgnd.org'}
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.sendgrid.com',
-      port:                 587,
-      domain:               'gdgnd.org',
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
       user_name:            ENV["GDG_SENDGRID_USERNAME"],
       password:             ENV["GDG_SENDGRID_PASSWORD"],
+      domain:               'gdgnd.org',
+      address:              'smtp.sendgrid.net',
+      port:                 587,
       authentication:       'plain',
       enable_starttls_auto: true
   }
