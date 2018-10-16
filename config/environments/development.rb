7d59951d7fe0ef97f19e2193da58e9c7ab5a23f8
arpansac <arpansac@gmail.com>
2018-10-16 12:04:40 +0530

    setup sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,11 +71,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
+      address:              'smtp.sendgrid.com',
       port:                 587,
-      domain:               'localhost:3000',
-      user_name:            'gdgnewdelhi@gmail.com',
-      password:             '***REMOVED***',
+      domain:               'gdgnd.org',
+      user_name:            ENV["GDG_SENDGRID_USERNAME"],
+      password:             ENV["GDG_SENDGRID_PASSWORD"],
       authentication:       'plain',
       enable_starttls_auto: true
   }
