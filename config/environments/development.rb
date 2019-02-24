0188048ce5f04cd32af0e23657c7b5387a3b2c70
arpansac <arpansac@gmail.com>
2019-02-24 21:46:36 +0530

    Revert "Update development.rb"
    This reverts commit 5dcfacb720f40a3e43d13d398a3522e4ddc61a21.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,9 +60,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
-  #when setting up the project locally 
-  config.active_storage.service = :local
+
+  config.active_storage.service = :amazon
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
